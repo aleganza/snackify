b35dc34d663283793b6c880706b6d332eacaaa1b
aleganza <ganzarolli.alessio@gmail.com>
2025-03-25 02:18:43 +0100

    feat: rust invoked function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,23 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello {name}, You have been greeted from Rust!")
+}
 
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
-    .run(context)
-    .expect("error while running tauri application");
+    let context = tauri::generate_context!();
+
+    tauri::Builder::default()
+        .setup(|app| {
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(context)
+        .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,14 @@
 <script>
-  let nomePlaylist = '';
+  import { invoke } from "@tauri-apps/api/core";
+
+  async function chiamaRust() {
+    const message = "ale"
+    const greeting = await invoke('greet', { name: message });
+
+    console.log(greeting)
+  }
 </script>
 
 <main class="p-8">
-  <div class="card w-96 bg-base-100 shadow-xl">
-    <figure><img src="https://via.placeholder.com/400x200" alt="Immagine Playlist" /></figure>
-    <div class="card-body">
-      <h2 class="card-title">Crea una nuova Playlist</h2>
-      <p>Seleziona le canzoni preferite e crea una nuova playlist personalizzata.</p>
-
-      <input 
-        type="text" 
-        placeholder="Nome Playlist" 
-        class="input input-bordered w-full" 
-        bind:value={nomePlaylist} 
-      />
-
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary" on:click={() => alert(`Playlist ${nomePlaylist} creata!`)}>Crea Playlist</button>
-      </div>
-    </div>
-  </div>
-
+  <button class="btn btn-primary" on:click={chiamaRust}>Saluta</button>
 </main>
