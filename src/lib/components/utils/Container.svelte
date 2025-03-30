3efb865a17941841be347fd69bb7a083ad10c276
aleganza <ganzarolli.alessio@gmail.com>
2025-03-30 22:36:36 +0200

    feat: logo
    [skip netlify]

--- src/lib/components/utils/Container.svelte
+++ src/lib/components/utils/Container.svelte
@@ -1,6 +1,9 @@
-<div
-  class="flex flex-col items-center justify-center min-h-screen bg-base-200"
->
+<script>
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen bg-base-200">
+  <img src="/logo.png" alt="logo" class="w-[70px] h-[70px] filter drop-shadow-[0_0_5px_rgba(255,255,255,0.25)] mb-3" />
+
   <h1 class="text-3xl font-bold mb-2">Snackify</h1>
 
   <p class="text-sm opacity-50 mb-5 max-w-[350px] text-center">
